6c1f504c4e4e469bd99ab2bcc945b31c1b5e2ec8
WeiszRobert <weiszrobi02@gmail.com>
2023-12-16 14:49:33 +0100

    day 16

---
+++ 2023/16/task2.js
@@ -0,0 +1,149 @@
+const path = require('path');
+const fs = require('fs');
+
+const input = fs
+	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+	.toString()
+	.trim()
+	.split('\r\n')
+
+
+function runAlgorithm(startingBeam) {
+    let visited = []
+    for(let i = 0; i < input.length; i++) {
+        visited[i] = []
+        for(let j = 0; j < input[0].length; j++) {
+            visited[i][j] = []
+        }
+    }
+    let tick = 0
+    let beams = []
+    beams.push(startingBeam)
+
+    while (beams.length > 0) {
+        tick++
+        let newBeams = []
+        let beamToRemove = []
+        for (let i = 0; i < beams.length; i++) {
+            let beam = beams[i]
+            let x = beam.x
+            let y = beam.y
+            let direction = beam.direction
+    
+            switch (direction) {
+                case 'right':
+                    y++
+                    break;
+                case 'left':
+                    y--
+                    break;
+                case 'up':
+                    x--
+                    break;
+                case 'down':
+                    x++
+                    break;
+            }
+    
+            if (tick > 1 && visited[beam.x][beam.y].includes(direction)) {
+                beamToRemove.push(i)
+                continue
+            } else if (tick > 1) {
+                visited[beam.x][beam.y].push(direction)
+            }
+    
+            if (x < 0 || x >= input.length || (tick > 1 && y < 0) || y >= input[0].length) {
+                beamToRemove.push(i)
+                continue
+            }
+    
+            if (input[x][y] === '.') {
+                beams[i] = { x, y, direction }
+            } else if (input[x][y] === '/') {
+                switch (direction) {
+                    case 'right':
+                        direction = 'up'
+                        break;
+                    case 'left':
+                        direction = 'down'
+                        break;
+                    case 'up':
+                        direction = 'right'
+                        break;
+                    case 'down':
+                        direction = 'left'
+                        break;
+                }
+                beams[i] = { x, y, direction }
+            } else if (input[x][y] === '\\') {
+                switch (direction) {
+                    case 'right':
+                        direction = 'down'
+                        break;
+                    case 'left':
+                        direction = 'up'
+                        break;
+                    case 'up':
+                        direction = 'left'
+                        break;
+                    case 'down':
+                        direction = 'right'
+                        break;
+                }
+                beams[i] = { x, y, direction }
+            } else if (input[x][y] === '|') {
+                if (direction === 'left' || direction === 'right') {
+                    newBeams.push({ x, y, direction: 'up' })
+                    newBeams.push({ x, y, direction: 'down' })
+                    beamToRemove.push(i)
+                } else {
+                    beams[i] = { x, y, direction }
+                }
+            } else if (input[x][y] === '-') {
+                if (direction === 'up' || direction === 'down') {
+                    newBeams.push({ x, y, direction: 'left' })
+                    newBeams.push({ x, y, direction: 'right' })
+                    beamToRemove.push(i)
+                } else {
+                    beams[i] = { x, y, direction }
+                }
+            }
+    
+        }
+        beams = beams.filter((_, index) => !beamToRemove.includes(index))
+        beams = beams.concat(newBeams)
+    }
+    
+    let count = 0
+    for(let i = 0; i < input.length; i++) {
+        for(let j = 0; j < input[0].length; j++) {
+            if (visited[i][j].length >= 1) {
+                count++
+            }
+        }
+    }
+    return count
+}
+
+let edges = [
+    { length: input[0].length, fixedValue: 0, isRow: true, direction: 'down' },
+    { length: input[0].length, fixedValue: input.length - 1, isRow: true, direction: 'up' },
+    { length: input.length, fixedValue: 0, isRow: false, direction: 'right' },
+    { length: input.length, fixedValue: input[0].length - 1, isRow: false, direction: 'left' }
+];
+
+function runOnEdge(length, fixedValue, isRow, direction) {
+    let max = 0
+    for (let i = 0; i < length; i++) {
+        let beam = isRow ? { x: fixedValue, y: i, direction } : { x: i, y: fixedValue, direction }
+        if (runAlgorithm(beam) > max) {
+            max = runAlgorithm(beam)
+        }
+    }
+    return max
+}
+
+let results = edges.map(edge => runOnEdge(edge.length, edge.fixedValue, edge.isRow, edge.direction));
+
+let max = Math.max(...results)
+console.log(max)
